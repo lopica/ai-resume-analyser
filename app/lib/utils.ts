df6d81ca6a1d38cfad1288a10f607c4ffadb7963
lopica <ducanhedison@gmail.com>
2025-07-30 15:17:28 +0700

    resume feedback component

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,6 @@
+import {twMerge} from "tailwind-merge";
+import {type ClassValue, clsx} from "clsx";
+
 export function formatSize(bytes: number): string {
     if(bytes === 0) return "0 Bytes";
 
@@ -10,3 +13,7 @@ export function formatSize(bytes: number): string {
 }
 
 export const generateUUID = () => crypto.randomUUID()
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
